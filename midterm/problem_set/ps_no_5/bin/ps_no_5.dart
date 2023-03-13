fbecbf32485ba738e74cd57fd03bb9f3ba64dbc3
Wilbert Flores Jr <didihearasqeak@gmail.com>
2023-03-13 19:16:57 +0800

    commit

---
+++ midterm/problem_set/ps_no_5/bin/ps_no_5.dart
@@ -0,0 +1,13 @@
+//problem set no. 5 
+//march 13, 2023
+//Create a function that calculates the sum of numbers within an array.
+
+import 'dart:io';
+import 'dart:convert';
+import 'dart:math';
+
+void main() {
+  final list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  final sum = list.sum;
+  print(sum);
+}

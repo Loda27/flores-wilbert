2271516ac679a76604929c1dc249226d8a67a5e8
Wilbert T Flores Jr <123954008+Loda27@users.noreply.github.com>
2023-05-13 20:11:54 +0800

    add ps# 9

---
+++ semifinals/ps_no_9/bin/ps_no_9.dart
@@ -0,0 +1,5 @@
+import 'package:ps_no_9/ps_no_9.dart' as ps_no_9;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ps_no_9.calculate()}!');
+}
---
+++ semifinals/ps_no_9/test/ps_no_9_test.dart
@@ -0,0 +1,8 @@
+import 'package:ps_no_9/ps_no_9.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
